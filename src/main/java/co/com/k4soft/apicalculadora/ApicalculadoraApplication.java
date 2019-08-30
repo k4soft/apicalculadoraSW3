4f6e3c580ef84fd0a27143a1ffb4694a0414be2f
diegogarcia <consultor@dgpragmatic.com>
2019-08-29 19:42:48 -0500

    Initial commit

---
+++ src/main/java/co/com/k4soft/apicalculadora/ApicalculadoraApplication.java
@@ -0,0 +1,13 @@
+package co.com.k4soft.apicalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicalculadoraApplication.class, args);
+	}
+
+}
